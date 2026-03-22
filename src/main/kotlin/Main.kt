5b8a2d7aa3e06bc27edeabef152cf216ad047f99
waimea-ldhunt <ldhunt@waimea.school.nz>
2026-03-23 12:27:01 +1300

    Added Health-bars and Setting up Locations

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -29,28 +29,19 @@ fun main() {
  */
 class Game {
     var name = "Test"
-    var score = 0
-    var health = 100
     var distanceTravelled = 0
+    var health = 100
 
-    val cards = listOf( // (Name,image,damage,defence,healing,speed)
-        listOf("Axe", "axe.png", 7, 3, 0, 0),
-        listOf("Shield", "shield.png", 0, 10),
-        listOf("Health Potion", "shield.png", 0, 0, 50, 0),
-        listOf("Speed Potion", "shield.png", 0, 0, 0, 5)
+    val village = Location("Village", "village.png")
+
+    val location = village
+
+    val cards = listOf( // (Name,image,effect,value)
+        listOf("Axe", "axe.png", "DMG", 7),
+        listOf("Shield", "shield.png", "DEF", 10),
+        listOf("Health Potion", "red_potion.png", "HEAL", 50),
+        listOf("Speed Potion", "blue_potion.png", "SPD", 5)
     )
-
-    fun scorePoints(points: Int) {
-        score += points
-    }
-
-    fun resetScore() {
-        score = 0
-    }
-
-    fun maxScoreReached(): Boolean {
-        return score >= 10000
-    }
 }
 
 
@@ -61,18 +52,16 @@ class Game {
  */
 class MainWindow(val game: Game) {
     val frame = JFrame("WINDOW TITLE")
-    private val panel = JPanel().apply { layout = null }
+    private val pane = JLayeredPane().apply { layout = null }
 
     private val locationLabel = JLabel()
-    private val locationIcon = ImageIcon(ClassLoader.getSystemResource("images/village.png"))
+    private val locationIcon = ImageIcon(game.location.icon)
 
-    private val titleLabel = JLabel("APP TITLE")
+    private val healthBar = JProgressBar(0, 100)
+    private val healthBarLabel = JLabel("Health")
 
-    private val infoLabel = JLabel()
-    private val clickButton = JButton("Click Me!")
-    private val infoButton = JButton("Info")
-
-    private val infoWindow = InfoWindow(this, game)      // Pass app state to dialog too
+    private val enemyBar = JProgressBar(0, 100)
+    private val enemyBarLabel = JLabel("Enemy Health")
 
     init {
         setupLayout()
@@ -83,63 +72,56 @@ class MainWindow(val game: Game) {
     }
 
     private fun setupLayout() {
-        panel.preferredSize = java.awt.Dimension(1600, 420)
-
-        titleLabel.setBounds(30, 30, 340, 30)
-        infoLabel.setBounds(30, 90, 340, 30)
-        clickButton.setBounds(30, 150, 240, 40)
-        infoButton.setBounds(300, 150, 70, 40)
+        pane.preferredSize = java.awt.Dimension(1600, 420)
 
         locationLabel.setBounds(500, 10, 600, 400)
-        panel.add(locationLabel)
+        healthBar.setBounds(10, 10, 480, 50)
+        healthBarLabel.setBounds(10, 10, 480, 50)
+
+        enemyBar.setBounds(1110, 10, 480, 50)
+        enemyBarLabel.setBounds(1110, 10, 480, 50)
+
+        pane.add(locationLabel)
+        pane.add(healthBar)
+        pane.add(healthBarLabel, JLayeredPane.DEFAULT_LAYER + 1)
+        pane.add(enemyBar)
+        pane.add(enemyBarLabel, JLayeredPane.DEFAULT_LAYER + 1)
+
     }
 
     private fun setupStyles() {
-        titleLabel.font = Font(Font.SANS_SERIF, Font.BOLD, 32)
-        infoLabel.font = Font(Font.SANS_SERIF, Font.PLAIN, 20)
-
-        clickButton.font = Font(Font.SANS_SERIF, Font.PLAIN, 20)
-        clickButton.background = Color(0xcc0055)
-
-        infoButton.font = Font(Font.SANS_SERIF, Font.PLAIN, 20)
-
         locationLabel.icon = locationIcon
+
+        healthBar.value = 100
+        healthBar.foreground = Color.RED
+
+        healthBarLabel.horizontalAlignment = SwingConstants.CENTER
+        healthBarLabel.font = Font("SANS_SERIF", Font.BOLD, 20)
+
+        enemyBar.value = 100
+        enemyBar.foreground = Color.RED
+        enemyBar.isVisible = false
+
+        enemyBarLabel.horizontalAlignment = SwingConstants.CENTER
+        enemyBarLabel.font = Font("SANS_SERIF", Font.BOLD, 20)
+        enemyBarLabel.isVisible = false
     }
 
     private fun setupWindow() {
         frame.isResizable = false                           // Can't resize
         frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE  // Exit upon window close
-        frame.contentPane = panel                           // Define the main content
+        frame.contentPane = pane                           // Define the main content
         frame.pack()
         frame.setLocationRelativeTo(null)                   // Centre on the screen
     }
 
     private fun setupActions() {
-        clickButton.addActionListener { handleMainClick() }
-        infoButton.addActionListener { handleInfoClick() }
+
     }
 
-    private fun handleMainClick() {
-        game.scorePoints(1000)       // Update the app state
-        updateUI()                  // Update this window UI to reflect this
-    }
-
-    private fun handleInfoClick() {
-        infoWindow.show()
-    }
 
     fun updateUI() {
-        infoLabel.text = "User ${game.name} has ${game.score} points"
 
-        if (game.maxScoreReached()) {
-            clickButton.text = "No More!"
-            clickButton.isEnabled = false
-        } else {
-            clickButton.text = "Click Me!"
-            clickButton.isEnabled = true
-        }
-
-        infoWindow.updateUI()       // Keep child dialog window UI up-to-date too
     }
 
     fun show() {
@@ -153,75 +135,16 @@ class MainWindow(val game: Game) {
  * app state can be shown / updated from multiple places
  *
  * @param owner the parent frame, used to position and layer the dialog correctly
- * @param app the app state object
+ * @param game the app state object
  */
-class InfoWindow(val owner: MainWindow, val game: Game) {
-    private val dialog = JDialog(owner.frame, "DIALOG TITLE", false)
-    private val panel = JPanel().apply { layout = null }
 
-    private val infoLabel = JLabel()
-    private val resetButton = JButton("Reset")
-
-    init {
-        setupLayout()
-        setupStyles()
-        setupActions()
-        setupWindow()
-        updateUI()
-    }
-
-    private fun setupLayout() {
-        panel.preferredSize = java.awt.Dimension(240, 180)
-
-        infoLabel.setBounds(30, 30, 180, 60)
-        resetButton.setBounds(30, 120, 180, 30)
-
-        panel.add(infoLabel)
-        panel.add(resetButton)
-    }
-
-    private fun setupStyles() {
-        infoLabel.font = Font(Font.SANS_SERIF, Font.PLAIN, 16)
-        resetButton.font = Font(Font.SANS_SERIF, Font.PLAIN, 16)
-    }
-
-    private fun setupWindow() {
-        dialog.isResizable = false                              // Can't resize
-        dialog.defaultCloseOperation = JDialog.HIDE_ON_CLOSE    // Hide upon window close
-        dialog.contentPane = panel                              // Main content panel
-        dialog.pack()
-    }
-
-    private fun setupActions() {
-        resetButton.addActionListener { handleResetClick() }
-    }
-
-    private fun handleResetClick() {
-        game.resetScore()    // Update the app state
-        owner.updateUI()    // Update the UI to reflect this, via the main window
-    }
-
-    fun updateUI() {
-        // Use app properties to display state
-        infoLabel.text = "<html>User: ${game.name}<br>Score: ${game.score} points"
-
-        resetButton.isEnabled = game.score > 0
-    }
-
-    fun show() {
-        val ownerBounds = owner.frame.bounds          // get location of the main window
-        dialog.setLocation(                           // Position next to main window
-            ownerBounds.x + ownerBounds.width + 10,
-            ownerBounds.y
-        )
-
-        dialog.isVisible = true
-    }
-}
-
-class Card(game: Game) {
+class Card(owner: MainWindow, game: Game) {
 
     init {
         val name = game.cards[Random.nextInt(game.cards.size)][0]
     }
+}
+
+class Location(val name: String, val image: String) {
+    val icon = ClassLoader.getSystemResource("images/$image")
 }
